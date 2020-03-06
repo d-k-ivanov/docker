1d6405a5a4b0b0ce083f176a6816386f48e33dfa
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-03-06 17:13:17 +0300

    change browsermob-proxy runtime contaner name

--- browsermob-proxy-python/scripts/run.ps1
+++ browsermob-proxy-python/scripts/run.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 ${RUNPATH} = (get-item $PSScriptRoot ).parent.FullName
 ${VERSION} = $(Get-Content ${RUNPATH}\version -First 1)
-docker run --rm -it --name="browsermob-proxy-py" keepbot/browsermob-proxy-py:${VERSION} @args
+docker run --rm -it --name="browsermob-proxy-py" -p 8090:8090 keepbot/browsermob-proxy-py:${VERSION} @args
